ffe75d15daf4a52c81e3749b2772d6b266f5171b
Micah Cantor <micah@fullcodemedical.com>
2025-12-31 00:47:41 -0500

    css improvements

--- src/components/ArticleCard.tsx
+++ src/components/ArticleCard.tsx
@@ -15,23 +15,25 @@ export function ArticleCard({ page }: ArticleCardProps) {
 	});
 
 	return (
-		<a href={page.url} className="w-full">
-			<div className="w-full px-4 py-2 flex flex-col rounded-lg border shadow-sm hover:shadow-violet-300 dark:hover:shadow-violet-700 dark:bg-slate-800 dark:shadow-slate-700 border-slate-300 dark:border-slate-800">
-				<div className="flex flex-col pb-1 lg:flex-row lg:justify-between lg:pt-1 lg:gap-2">
-					<h2 className="font-medium text-lg">
-						{articleExports.metadata.title}
-					</h2>
-					<time dateTime={date.toDateString()} className="text-sm text-nowrap">
-						{formattedDate}
-					</time>
+		<div className="w-full flex flex-col">
+			<a href={page.url} className="w-full">
+				<div className="w-full px-4 py-2 flex flex-col rounded-lg rounded-b-none border border-slate-300 dark:border-slate-700 active:bg-slate-200 dark:active:bg-slate-700 dark:bg-slate-800">
+					<div className="flex flex-col pb-1 lg:flex-row lg:justify-between lg:pt-1 lg:gap-2">
+						<h2 className="font-medium text-lg">
+							{articleExports.metadata.title}
+						</h2>
+						<time dateTime={date.toDateString()} className="text-sm text-nowrap">
+							{formattedDate}
+						</time>
+					</div>
+					<p>{articleExports.metadata.description}</p>
 				</div>
-				<p className="pb-1.5">{articleExports.metadata.description}</p>
-				<div className="flex flex-row space-x-2">
-					{articleExports.metadata.tags.map((tag) => (
-						<ArticleTag key={tag} value={tag} />
-					))}
-				</div>
+			</a>
+			<div className="flex flex-row py-2 px-3 space-x-2 rounded-b-lg border border-t-0 border-slate-300 dark:border-slate-700 dark:bg-slate-800">
+				{articleExports.metadata.tags.map((tag) => (
+					<ArticleTag key={tag} value={tag} />
+				))}
 			</div>
-		</a>
+		</div>
 	);
 }
